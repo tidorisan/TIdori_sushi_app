01eaca7b48d8256fdc4f5a1aa6b230f1ad96cbc3
vagrant <vagrant@localhost.localdomain>
2020-08-28 06:08:29 +0000

    [create]login_signup_function

--- app/models/store.rb
+++ app/models/store.rb
@@ -1,2 +1,6 @@
 class Store < ApplicationRecord
+
+	enum no_smoking: { full_smoking_allowed: 1, separating_smoking_areas: 2, full_no_smoking: 3 }
+
+	enum reservation: { available: 1, reservation_not_possible: 2, full_reservation_system: 3 }
 end
