6f936fbbb7166194918ea2be0cfaaebbfb2e714a
vagrant <vagrant@localhost.localdomain>
2020-08-28 06:54:35 +0000

    [create]store_equipment_function

--- app/controllers/users/store_equipments_controller.rb
+++ app/controllers/users/store_equipments_controller.rb
@@ -1,15 +1,33 @@
 class Users::StoreEquipmentsController < ApplicationController
   def index
+  	@equipment = Equipment.new
+  	@equipments = Equipment.all
   end
 
   def create
-  	
+     @equipment = Equipment.new(equipment_params)
+    if @equipment.save
+      redirect_to users_store_equipments_path
+    else
+      render 'users/store_equipments/index'
+    end
   end
 
   def edit
+  	@equipment = Equipment.find(params[:id])
   end
 
   def update
-  	
+  	@equipment = Equipment.find(params[:id])
+    if @equipment.update(equipment_params)
+      redirect_to users_store_equipments_path
+    else
+      render 'users/store_equipments/edit'
+    end
+  end
+
+  private
+  def equipment_params
+    params.require(:equipment).permit(:genre_name, :display_status)
   end
 end
