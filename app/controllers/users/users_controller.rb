bacae4b85a7fa6aaf261a23e10d3f669c70c7477
vagrant <vagrant@localhost.localdomain>
2020-08-29 10:11:10 +0000

    [createfeature_store_admin_unsubscribe_feature]

--- app/controllers/users/users_controller.rb
+++ app/controllers/users/users_controller.rb
@@ -3,10 +3,28 @@ class Users::UsersController < ApplicationController
   end
 
   def create
-  	
+
   end
 
   def confirmation
-  	
+
+  end
+
+  def leave
+  end
+
+  def unsubscribe_update
+  	@user = User.find(current_user.id)
+  	if @user.update(unsubscribe_status: false)
+  		reset_session
+  		redirect_to users_homes_path
+  	else
+  		render '/users/users/leave'
+  	end
+  end
+
+  private
+  def user_params
+  	params.require(:user).permit(:unsubscribe_status)
   end
 end
