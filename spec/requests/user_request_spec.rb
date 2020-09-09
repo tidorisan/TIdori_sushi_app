834ccc2c24724f9e28466c873de468d74325251f
vagrant <vagrant@localhost.localdomain>
2020-09-09 01:22:37 +0000

    [fix]conflict

--- spec/requests/user_request_spec.rb
+++ spec/requests/user_request_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
+  describe '顧客利用者画面の表示' do
+    context "顧客利用者画面正しく表示される" do
+      before do
+        get posts_new_path
+      end
+      it 'リクエストは200 OKとなること' do
+        expect(response.status).to eq 200
+      end
+    end
+  end
 
 end
