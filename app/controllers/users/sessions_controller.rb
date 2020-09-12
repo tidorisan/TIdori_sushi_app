6d851273eb48d55f10f6b943a97f5cb36119685c
vagrant <vagrant@localhost.localdomain>
2020-09-12 06:13:59 +0000

    [fix]user_home_top_page

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -38,7 +38,6 @@ class Users::SessionsController < Devise::SessionsController
 
   def new_guest
     user = User.guest
-    user.role ==  "customer"
     sign_in user
     redirect_to root_path
   end
